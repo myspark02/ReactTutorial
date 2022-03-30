149e5b7f6c415f6e2e5671d6ddafa764ad45b9e8
myspark02 <myspark02@gmail.com>
2022-03-31 00:53:38 +0900

    react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import React from "react";
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
-   
-    return null;
-  }
-
+  return (
+    <Router>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>}>
+        </Route>
+        <Route path="/movie" element={<Detail/>}>
+        </Route>
+        <Route path="/" element={<Home/>}>
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
 export default App;
