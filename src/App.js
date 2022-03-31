0673741e4eb579e72ddf0001c8f0f4d0452364e5
myspark02 <myspark02@gmail.com>
2022-04-01 01:25:56 +0900

    issues about publishing resolved

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/about-us" element={<h1>Hello</h1>}>
         </Route>
-        <Route path="/movie/:id" element={<Detail/>}>
+        <Route  path="/movie/:id" element={<Detail/>}>
         </Route>
-        <Route path="/" element={<Home/>}>
+        <Route  path="/" element={<Home/>}>
         </Route>
       </Routes>
     </Router>
