9931c2d4de55f6f01fb17f7bf0f596aca33e7a3e
myspark02 <myspark02@gmail.com>
2022-04-13 20:49:20 +0900

    useClick

---
+++ src/useClick/index.js
@@ -0,0 +1,24 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { useClick } from "./useClick";
+
+import "./styles.css";
+
+const App = () => {
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
+
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
