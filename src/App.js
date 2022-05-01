6bdd068b0a23c12dac3007473c61b317bc1bbff2
myspark02 <myspark02@gmail.com>
2022-05-01 18:20:13 +0900

    Simple notes using markdown editor

--- src/App.js
+++ src/App.js
@@ -1,17 +1,105 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Main from './components/Main';
+import React from "react"
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor"
+import Split from "react-split"
+import {nanoid} from "nanoid"
 
 export default function App() {
-    const [darkMode, setDarkMode] = React.useState(true);
-
-    function toggleDarkMode() {
-        setDarkMode(prevMode => !prevMode )
+    
+    const [notes, setNotes] = React.useState(
+        JSON.parse(localStorage.getItem("notes")) || []
+    )
+    const [currentNoteId, setCurrentNoteId] = React.useState(
+        (notes[0] && notes[0].id) || ""
+    )
+    
+    React.useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes))
+    }, [notes])
+    
+    function createNewNote() {
+        const newNote = {
+            id: nanoid(),
+            body: "# Type your markdown note's title here"
+        }
+        setNotes(prevNotes => [newNote, ...prevNotes])
+        setCurrentNoteId(newNote.id)
     }
+    
+    function updateNote(text) {
+        // setNotes(oldNotes => oldNotes.map(oldNote => {
+        //     return oldNote.id === currentNoteId
+        //         ? { ...oldNote, body: text }
+        //         : oldNote
+        // }))
+
+        // Try to rearrange the most recently modified 
+        setNotes(oldNotes => {
+            // create a new empty array, loop over the original array
+            // if the if matches with the current id, 
+            // put the updated note at the beginning of the new array
+            // else push the old note to the end of the new arry
+            const newArray = [];
+            oldNotes.forEach(oldNote => {
+                if(oldNote.id === currentNoteId) {
+                    newArray.unshift({...oldNote, body:text})
+                }else {
+                    newArray.push(oldNote);
+                }
+            });
+            return newArray;
+        })
+    }
+    
+    function findCurrentNote() {
+        return notes.find(note => {
+            return note.id === currentNoteId
+        }) || notes[0]
+    }
+    
+    function deleteNote(event, noteId) {
+        event.stopPropagation();
+        setNotes( prevNotes => prevNotes.filter(note => note.id !== noteId ))
+
+    };
     return (
-        <div className="container">
-            <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-            <Main darkMode={darkMode} />
-        </div>
+        <main>
+        {
+            notes.length > 0 
+            ?
+            <Split 
+                sizes={[30, 70]} 
+                direction="horizontal" 
+                className="split"
+            >
+                <Sidebar
+                    notes={notes}
+                    currentNote={findCurrentNote()}
+                    setCurrentNoteId={setCurrentNoteId}
+                    newNote={createNewNote}
+                    deleteNote={deleteNote}
+                />
+                {
+                    currentNoteId && 
+                    notes.length > 0 &&
+                    <Editor 
+                        currentNote={findCurrentNote()} 
+                        updateNote={updateNote} 
+                    />
+                }
+            </Split>
+            :
+            <div className="no-notes">
+                <h1>You have no notes</h1>
+                <button 
+                    className="first-note" 
+                    onClick={createNewNote}
+                >
+                    Create one now
+                </button>
+            </div>
+            
+        }
+        </main>
     )
 }
